d790002d60e0663adcef03a82fbb7c29c2e368c0
Gustav Pursche <pursche@posteo.de>
2016-10-28 22:01:15 +0200

    initial skeleton

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const webpack = require('webpack-stream');
+
+gulp.task('scripts', () => {
+  return gulp.src('assets/scripts/common.js')
+    .pipe(webpack({
+      entry: './assets/scripts/common.js',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: path.resolve(__dirname, 'node_modules'),
+            loader: 'babel',
+            query: {
+              presets: [
+                'es2015',
+              ],
+              plugins: [
+                'transform-runtime',
+              ],
+            },
+          },
+        ],
+      },
+      output: {
+        filename: 'common.js',
+      },
+      resolveLoader: {
+        root: path.resolve('./node_modules'),
+      },
+      resolve: {
+        root: [
+          path.resolve('./assets/scripts/'),
+        ]
+      }
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./assets/styles/common.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./dist/styles/'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./scripts/**/*', ['scripts']);
+  gulp.watch('./styles/**/*', ['styles']);
+});

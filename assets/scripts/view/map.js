eb1d75452710290a596b7ee29e84c14423ef6a25
Gustav Pursche <pursche@posteo.de>
2016-11-14 22:17:18 +0100

    map: add intro title

--- assets/scripts/view/map.js
+++ assets/scripts/view/map.js
@@ -1,10 +1,12 @@
+import _ from 'underscore';
+import i18n from 'lib/i18n';
 import L from 'leaflet';
 
 export default Backbone.View.extend({
   className: 'map',
 
   createMap() {
-    const map = L.map(this.$el.get(0), {
+    const map = L.map(this.$el.find('.map__container').get(0), {
       scrollWheelZoom: false,
     });
     map.setView([51.505, -0.09], 13);
@@ -19,8 +21,17 @@ export default Backbone.View.extend({
   },
 
   render() {
-    this.$el.html('');
+    this.$el.html(this.template({
+      i18n,
+    }));
     this.map = this.createMap();
     return this;
   },
+
+  template: _.template(`
+    <h1 class="map__title">
+      <%= i18n('Who gets paid to stop the worlds refugees?') %>
+    </h1>
+    <div class="map__container"></div>
+  `),
 });

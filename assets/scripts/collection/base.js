dadc03096cc8770a8027d90ecda322224010f4e6
Gustav Pursche <pursche@posteo.de>
2016-12-11 02:10:51 +0100

    collection/base: use API proxy

--- assets/scripts/collection/base.js
+++ assets/scripts/collection/base.js
@@ -1,23 +1,8 @@
+import API from 'lib/api';
 import Backbone from 'backbone';
 
 export default Backbone.Collection.extend({
-  getLanguage() {
-    return 'de';
-  },
-
-  getAPIVersion() {
-    return 'v1';
-  },
-
-  getAPINamespace() {
-    return 'migrationcontrol';
-  },
-
   getAPIEndpointURL(endpoint) {
-    const lang = this.getLanguage();
-    const version = this.getAPIVersion();
-    const ns = this.getAPINamespace();
-
-    return `http://localhost:8080/${ns}/${version}/${lang}/${endpoint}`;
+    return new API()._buildAPIUrl(endpoint);
   },
 });

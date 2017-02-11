14a29919808ceea8a323441d4e69a16796e0df8c
Gustav Pursche <pursche@posteo.de>
2017-02-11 13:11:28 +0100

    map: improve scaling of valetta payments on small screens

--- assets/scripts/model/map-country-payment.js
+++ assets/scripts/model/map-country-payment.js
@@ -49,7 +49,14 @@ export default MapContryBase.extend({
   /* calculate radius for an oda bubble for a single year */
   getRadius(year) {
     const type = 'singlePayments';
-    const range = this.getRange([25, 150]);
+    const windowWidth = $(window).width();
+    let rangeValues = [20, 120];
+
+    if (windowWidth > 768 ) {
+      rangeValues = [25, 150];
+    }
+
+    const range = this.getRange(rangeValues);
     const value = this._getDataValueForYear(type, year);
 
     if (!value) {
@@ -60,7 +67,16 @@ export default MapContryBase.extend({
   },
 
   getFontSize(type, year) {
-    const scaling = type === 'unit' ? [.6, .8] : [.9, 4];
+    let titleRangeValue = [.7, 2.5];
+    let unitRangeValue = [.4, .7];
+    const windowWidth = $(window).width();
+
+    if (windowWidth > 768) {
+      titleRangeValue = [.9, 4];
+      unitRangeValue = [.6, .8];
+    }
+
+    const scaling = type === 'unit' ? unitRangeValue : titleRangeValue;
     const range = this.getRange(scaling);
     const value = this._getDataValueForYear('singlePayments', year);
     return range(value);

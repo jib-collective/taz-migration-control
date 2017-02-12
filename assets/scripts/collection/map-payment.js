3b26f94ce3faeb98dcfa23009d0b300927c3657f
Gustav Pursche <pursche@posteo.de>
2017-02-12 12:21:46 +0100

    map: simplify payment layer

--- assets/scripts/collection/map-payment.js
+++ assets/scripts/collection/map-payment.js
@@ -29,24 +29,21 @@ export default BaseCollection.extend({
   load() {
     return this.options.api.fetch('countriesoverview')
       .then(data => {
-        const promises = [];
-
-        _.forEach(data, item => {
-          _.forEach(item.entries, overviewCountry => {
-            const promise = this.options.api.fetch(`country/${overviewCountry.id}`)
+        const promises = data.map(item => {
+          return item.entries.map(overview => {
+            return this.options.api.findCountryById(overview.id)
               .then(country => {
-                if (country.data && country.data.singlePayments) {
-                  const attrs = country;
-                  attrs.map = this.options.map;
-                  this.add(attrs);
+                if (!country.data || !country.data.singlePayments) {
+                  return undefined;
                 }
-              });
 
-            promises.push(promise);
+                country.map = this.options.map;
+                this.add(country);
+              });
           });
         });
 
-        return $.when.apply($, promises)
+        return $.when.apply($, _.flatten(promises))
           .then(data => {
             this.trigger('sync');
             return data;

e5ca9b73fe85d9c65af89dbed4d6a1a3b69aab25
Gustav Pursche <pursche@posteo.de>
2017-04-17 16:49:35 +0100

    Update mapbox source

--- assets/scripts/model/map.js
+++ assets/scripts/model/map.js
@@ -1,6 +1,6 @@
 export default Backbone.Model.extend({
   defaults: {
-    tileLayer: 'https://api.mapbox.com/styles/v1/gustavpursche/cizwj7ulj004v2rleivodl103/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3VzdGF2cHVyc2NoZSIsImEiOiJhVVRUaFV3In0.IdUObuDS1u0tzNNDvNpfKg',
+    tileLayer: 'https://api.mapbox.com/styles/v1/tazde/cj1maj4mm001z2slpd3qgudyc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGF6ZGUiLCJhIjoiY2l6OGt6aHR0MDAybTJxbnYxMXc1OWtxayJ9.wMcn343eMnTSJ806fZNihg',
 
     mapOptions: {
       attributionControl: false,

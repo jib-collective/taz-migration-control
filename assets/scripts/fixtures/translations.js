320567c74d4f2bfc558ebccfde6912be195dbd18
Gustav Pursche <pursche@posteo.de>
2017-06-06 22:49:23 +0100

    Add english translations

--- assets/scripts/fixtures/translations.js
+++ assets/scripts/fixtures/translations.js
@@ -1,189 +1,236 @@
 export default {
   refugeesInCountry: {
     de: 'Anzahl internationaler Geflüchter im Land (2015)',
+    en: 'Number of international refugees in the country (2015)',
   },
 
   applicationsForAsylum: {
     de: 'Asylanträge in der EU-28 gesamt seit 2000',
+    en: 'Applications for asylum in the EU since 2000',
   },
 
   refugeesOutsideCountry: {
     de: 'Ins Ausland Geflüchtete aus diesem Land (2015)',
+    en: 'Externally displaced persons from this country (2015)',
   },
 
   refugeesInEU: {
     de: 'In die EU geflüchtete aus diesem Land (2015)',
+    en: 'Refugees from this country in the EU (2015)',
   },
 
   idp: {
     de: 'Binnenvertriebene (2015)',
+    en: 'Internally displaced persons (2015)',
   },
 
   asylumApprovalRate: {
     de: 'Anerkennungsquote in der EU-28 gesamt (Prozent, 2015)',
+    en: 'Approved asylum applications in the EU (percent, 2015)',
   },
 
   countriesRepatriationAgreement: {
     de: 'Formale Rücknahmeabkommen',
+    en: 'Formal repratiation Agreements',
   },
 
   frontexCooperation: {
     de: 'Frontex-Kooperation',
+    en: 'Frontex Cooperation',
   },
 
   detentionCenter: {
     de: 'Internierungslager',
+    en: 'Detention center',
   },
 
   departureLegality: {
     de: 'Straftat illegale Ausreise',
+    en: 'Criminal offense illegal departure',
   },
 
   Countries: {
     de: 'Länder',
+    en: 'Countries',
   },
 
   Thesis: {
     de: 'These',
+    en: 'Thesis',
   },
 
   Theses: {
     de: 'Thesen',
+    en: 'Thesis',
   },
 
   Background: {
     de: 'Hintergrund',
+    en: 'Background',
   },
 
   'Migration Control': {
     de: 'Migrationskontrolle',
+    en: 'Migration Control',
   },
 
   'Follow on Facebook': {
     de: 'Auf facebook folgen',
+    en: 'Follow on Facebook',
   },
 
   'Follow on Twitter': {
     de: 'Auf twitter folgen',
+    en: 'Follow on Twitter',
   },
 
   'A project of': {
     de: 'Ein Projekt von',
+    en: 'A project of',
   },
 
   'taz, the daily newspaper': {
     de: 'taz, die Tageszeitung',
+    en: 'taz, die Tageszeitung',
   },
 
   'Human Development Index': {
     de: 'Human Development Index',
+    en: 'Human Development Index',
   },
 
   Remittances: {
     de: 'Rückueberweisungen',
+    en: 'Remittances',
   },
 
   'EU Money': {
     de: 'EU Geld',
+    en: 'EU funds',
   },
 
   'Read More': {
     de: 'Weiter lesen',
+    en: 'Read more',
   },
 
   'Show less': {
     de: 'Einklappen',
+    en: 'Show less',
   },
 
   Information: {
     de: 'Infos',
+    en: 'Information',
   },
 
   Treaties: {
     de: 'Abkommen',
+    en: 'Treaties',
   },
 
   Introduction: {
     de: 'Einleitung',
+    en: 'Introduction',
   },
 
   'Skip Video': {
     de: 'Video überspringen',
+    en: 'Skip Video',
   },
 
   yes: {
     de: 'ja',
+    en: 'yes',
   },
 
   no: {
     de: 'nein',
+    en: 'no',
   },
 
   negotiating: {
     de: 'Verhandlung',
+    en: 'Negotiation',
   },
 
   Photo: {
     de: 'Foto',
+    en: 'Photo',
   },
 
   singlePayments: {
     de: 'Zahlungen zur Verhinderung irregulärer Migration',
+    en: 'Payments to prevent illegal migration',
   },
 
   asylumFigures: {
     de: 'Asylanträge in der EU',
+    en: 'Asylum applications in the EU',
   },
 
   Authors: {
     de: 'AutorInnen',
+    en: 'Authors',
   },
 
   'International aid': {
     de: 'Internationale Hilfe aus EU-Staaten',
+    en: 'Official development aid from the EU/EU-states',
   },
 
   unknown: {
     de: 'unbekannt',
+    en: 'unknown',
   },
 
   sources: {
     de: 'Quellen',
+    en: 'Sources',
   },
 
   'Payments from EU countries': {
     de: 'Zahlungen aus EU-Staaten',
+    en: 'Payments from EU countries',
   },
 
   'Dollar per capita': {
     de: 'Dollar/ Kopf',
+    en: 'Dollar per capita',
   },
 
   'Show map': {
     de: 'Karte<br/>öffnen',
+    en: 'Show map',
   },
 
   Finding: {
     de: 'Länderinfo',
+    en: 'Country report',
   },
 
   Features: {
     de: 'Features',
+    en: 'Features',
   },
 
   'Applications for Asylum': {
     de: 'Asylanträge',
+    en: 'Asylum applications',
   },
 
   Mio: {
     de: 'Mio',
+    en: 'Millions',
   },
 
   informalTreatiesDescription: {
     de: 'Informelle Vereinbarungen mit Migrationsbezug',
+    en: 'Informal migration agreements',
   },
 
   mapTreaties: {
     de: 'Rücknahmeabkommen',
+    en: '[TODO]',
   },
 };
